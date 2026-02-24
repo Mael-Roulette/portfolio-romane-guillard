af9f47f3e354b459370c568fc8a9b8620843870f
Mael <maelroulette12@gmail.com>
2026-02-24 21:49:59 +0100

    Updated socials icon

--- inc/menus.php
+++ inc/menus.php
@@ -1,50 +1,74 @@
-<?php 
+<?php
 
 /**
  * Ajout de menu dans l'admin
  */
-function register_menu () {
-  register_nav_menus( array (
-    'main'    => 'Menu principal',
-    'footer'  => 'Pied de page',
-    'legal'   => 'Menu légal',
-    'socials' => 'Réseaux sociaux'
-  ) );
+function register_menu()
+{
+    register_nav_menus([
+        "main" => "Menu principal",
+        "footer" => "Pied de page",
+        "legal" => "Menu légal",
+        "socials" => "Réseaux sociaux",
+    ]);
 }
 
-add_action( 'init', 'register_menu' );
+add_action("init", "register_menu");
 
-function social_links_to_icon ( $item_output, $item, $depth, $args ) {
-  if ( 'socials' === $args->theme_location ) {
-    $icon = '<span class="fas fa-link"></span>';
-    $icon = get_social_icons( $item->title );
+function social_links_to_icon($item_output, $item, $depth, $args)
+{
+    if ("socials" === $args->theme_location) {
+        $icon = get_icon($item->title);
 
-    $item_output = str_replace( $args->link_after, '</span>' . $icon, $item_output );
-  }
+        $item_output = str_replace(
+            $args->link_after,
+            "</span>" . $icon,
+            $item_output,
+        );
+    }
 
-  return $item_output;
+    return $item_output;
 }
 
-add_action( 'walker_nav_menu_start_el', 'social_links_to_icon', 10, 4 );
+add_action("walker_nav_menu_start_el", "social_links_to_icon", 10, 4);
 
 /**
  * Récupération des icones pour le menu des réseaux sociaux
  */
 
-  function get_social_icons ( $key ) {
-    $social_icons = array (
-      'instagram'  => '<span class="fa-brands fa-instagram"></span>',
-      'facebook'   => '<span class="fa-brands fa-facebook"></span>',
-      'youtube'    => '<span class="fa-brands fa-youtube"></span>',
-      'x'          => '<span class="fa-brands fa-x-twitter"></span>',
-      'linkedin'   => '<span class="fa-brands fa-linkedin"></span>',
-      'spotify'   => '<span class="fa-brands fa-spotify"></span>',
-    );
+function get_icon($name, $class = "")
+{
+    $icons = [
+        "youtube" =>
+            '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M23.5 6.2a3 3 0 0 0-2.1-2.1C19.5 3.5 12 3.5 12 3.5s-7.5 0-9.4.6A3 3 0 0 0 .5 6.2 31 31 0 0 0 0 12a31 31 0 0 0 .5 5.8 3 3 0 0 0 2.1 2.1c1.9.6 9.4.6 9.4.6s7.5 0 9.4-.6a3 3 0 0 0 2.1-2.1A31 31 0 0 0 24 12a31 31 0 0 0-.5-5.8zM9.7 15.5V8.5l6.3 3.5-6.3 3.5z"/></svg>',
+        "instagram" =>
+            '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M12 2.2c3.2 0 3.6 0 4.9.1 3.3.1 4.8 1.7 4.9 4.9.1 1.3.1 1.6.1 4.8 0 3.2 0 3.6-.1 4.8-.1 3.2-1.7 4.8-4.9 4.9-1.3.1-1.6.1-4.9.1-3.2 0-3.6 0-4.8-.1-3.3-.1-4.8-1.7-4.9-4.9C2.2 15.6 2.2 15.2 2.2 12c0-3.2 0-3.6.1-4.8C2.4 3.9 4 2.3 7.2 2.3c1.2-.1 1.6-.1 4.8-.1zM12 0C8.7 0 8.3 0 7.1.1 2.7.3.3 2.7.1 7.1.0 8.3 0 8.7 0 12c0 3.3 0 3.7.1 4.9.2 4.4 2.6 6.8 7 7C8.3 24 8.7 24 12 24c3.3 0 3.7 0 4.9-.1 4.4-.2 6.8-2.6 7-7 .1-1.2.1-1.6.1-4.9 0-3.3 0-3.7-.1-4.9C23.7 2.7 21.3.3 16.9.1 15.7 0 15.3 0 12 0zm0 5.8a6.2 6.2 0 1 0 0 12.4A6.2 6.2 0 0 0 12 5.8zm0 10.2a4 4 0 1 1 0-8 4 4 0 0 1 0 8zm6.4-11.8a1.4 1.4 0 1 0 0 2.8 1.4 1.4 0 0 0 0-2.8z"/></svg>',
+        "linkedin" =>
+            '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M20.4 20.4h-3.6v-5.6c0-1.3 0-3-1.8-3-1.9 0-2.1 1.4-2.1 2.9v5.7H9.3V9h3.4v1.6h.1c.5-.9 1.6-1.8 3.4-1.8 3.6 0 4.2 2.4 4.2 5.5v6.1zM5.3 7.4a2.1 2.1 0 1 1 0-4.2 2.1 2.1 0 0 1 0 4.2zM7.1 20.4H3.5V9h3.6v11.4zM22.2 0H1.8C.8 0 0 .8 0 1.7v20.6C0 23.2.8 24 1.8 24h20.4c1 0 1.8-.8 1.8-1.7V1.7C24 .8 23.2 0 22.2 0z"/></svg>',
+        "spotify" =>
+            '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M12 0C5.4 0 0 5.4 0 12s5.4 12 12 12 12-5.4 12-12S18.6 0 12 0zm5.5 17.3c-.2.3-.6.4-1 .2-2.6-1.6-5.9-2-9.8-1.1-.4.1-.8-.2-.8-.6-.1-.4.2-.8.6-.8 4.2-1 7.9-.6 10.8 1.2.4.3.5.7.2 1.1zm1.5-3.3c-.3.4-.8.5-1.2.3-3-1.8-7.5-2.3-11-1.3-.5.1-.9-.2-1-.6-.1-.5.2-.9.6-1 4-.1 8.9.5 12.3 2.4.4.2.5.8.3 1.2zm.1-3.4C15.6 8.4 9.9 8.2 6.8 9.1c-.5.2-1.1-.1-1.3-.7-.2-.5.1-1.1.7-1.3 3.6-1 9.8-.8 13.7 1.5.5.3.6.9.4 1.4-.3.5-.9.6-1.2.6z"/></svg>',
+        "email" =>
+            '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M20 4H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z"/></svg>',
+        "film" =>
+            '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M18 3v2h-2V3H8v2H6V3H4v18h2v-2h2v2h8v-2h2v2h2V3h-2zM8 17H6v-2h2v2zm0-4H6v-2h2v2zm0-4H6V7h2v2zm10 8h-2v-2h2v2zm0-4h-2v-2h2v2zm0-4h-2V7h2v2z"/></svg>',
+        "play" =>
+            '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M8 5v14l11-7z"/></svg>',
+        "user" =>
+            '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M12 12c2.7 0 4.8-2.1 4.8-4.8S14.7 2.4 12 2.4 7.2 4.5 7.2 7.2 9.3 12 12 12zm0 2.4c-3.2 0-9.6 1.6-9.6 4.8v2.4h19.2v-2.4c0-3.2-6.4-4.8-9.6-4.8z"/></svg>',
+        "phone" =>
+            '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M6.6 10.8c1.4 2.8 3.8 5.1 6.6 6.6l2.2-2.2c.3-.3.7-.4 1-.2 1.1.4 2.3.6 3.6.6.6 0 1 .4 1 1V20c0 .6-.4 1-1 1-9.4 0-17-7.6-17-17 0-.6.4-1 1-1h3.5c.6 0 1 .4 1 1 0 1.3.2 2.5.6 3.6.1.3 0 .7-.2 1L6.6 10.8z"/></svg>',
+    ];
 
-    foreach ( array_keys( $social_icons ) as $icon ) {
-      if ( $key === $icon ) {
-        return $social_icons[$icon];
-      }
+    $svg = isset($icons[$name]) ? $icons[$name] : "";
+
+    if ($class) {
+        $svg = str_replace(
+            "<svg ",
+            '<svg class="' . esc_attr($class) . '" ',
+            $svg,
+        );
     }
-  }
+
+    return $svg;
+}
 ?>
